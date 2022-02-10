f7a190808ee67a2d44153c199b5e2f2dcd9a0478
SonicoGO <song.xv@outlook.com>
2022-02-10 17:02:36 +0800

    finish hw2

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -3,5 +3,75 @@ package hw2;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
+    private WeightedQuickUnionUF WQU;
+    private int[] openSites;
+    private int N;
+    private int openNums;
 
+    // create N-by-N grid, with all sites initially blocked
+    public Percolation(int N){
+        this.N = N;
+        openNums = 0;
+        openSites = new int[N * N];
+        WQU = new WeightedQuickUnionUF(N * N + 2);
+    }
+
+    // open the site (row, col) if it is not open already
+    public void open(int row, int col){
+        int m = row * N + col;
+        if (!isOpen(row, col)) {
+            openSites[m] = 1;
+            openNums++;
+            unionOpen(m);
+        }
+    }
+
+    // uion blocks if needed
+    public void unionOpen(int m){
+        if (m < N && !WQU.connected(m, N * N)) {
+            WQU.union(m, N * N);
+        }
+        if (m >= N * N - N && !WQU.connected(m, N * N + 1)) {
+            WQU.union(m, N * N + 1);
+        }
+        if (m - N >= 0 && openSites[m - N] == 1) {
+            WQU.union(m, m - N);
+        }
+        if (m - 1 >= 0 && openSites[m - 1] == 1) {
+            WQU.union(m, m - 1);
+        }
+        if (m + N <= N * N - 1 && openSites[m + N] == 1) {
+            WQU.union(m, m + N);
+        }
+        if (m + 1 <= N * N - 1 && openSites[m + 1] == 1) {
+            WQU.union(m, m + 1);
+        }
+    }
+
+    // is the site (row, col) open?
+    public boolean isOpen(int row, int col){
+        int m = row * N + col;
+        return openSites[m] == 1;
+    }
+
+    // is the site (row, col) full?
+    public boolean isFull(int row, int col){
+        int m = row * N + col;
+        return WQU.connected(N * N, m);
+    }
+
+    // number of open sites
+    public int numberOfOpenSites(){
+        return openNums;
+    }
+
+    // does the system percolate?
+    public boolean percolates(){
+        return WQU.connected(N * N, N * N + 1);
+    }
+
+    // use for unit testing (not required)
+    public static void main(String[] args){
+
+    }
 }
--- hw2/hw2/PercolationStats.java
+++ hw2/hw2/PercolationStats.java
@@ -1,5 +1,42 @@
 package hw2;
 
-public class PercolationStats {
+import edu.princeton.cs.algs4.StdRandom;
 
+public class PercolationStats {
+    private double[] openNums;
+    private int T;
+    private int N;
+    // perform T independent experiments on an N-by-N grid
+    public PercolationStats(int N, int T, PercolationFactory pf){
+        this.T = T;
+        this.N = N;
+        openNums = new double[T];
+        for (int i = 0; i < T; i++) {
+            Percolation ufs = pf.make(N);
+            while(!ufs.percolates()) {
+                ufs.open(StdRandom.uniform(0, N), StdRandom.uniform(0, N));
+            }
+            openNums[i] = 1.0 * ufs.numberOfOpenSites() / N / N;
+        }
+    }
+    // sample mean of percolation threshold
+    public double mean(){
+        double sum = 0;
+        for (int i = 0; i < T ; i++) {
+            sum += openNums[i];
+        }
+        return sum / T;
+    }
+    // sample standard deviation of percolation threshold
+    public double stddev(){
+        return 0.0;
+    }
+    // low endpoint of 95% confidence interval
+    public double confidenceLow(){
+        return 0.0;
+    }
+    // high endpoint of 95% confidence interval
+    public double confidenceHigh(){
+        return 0.0;
+    }
 }
